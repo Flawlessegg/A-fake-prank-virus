df9ee846b0c042d054ab857640e4e7ef63c4cdd9
Flawlessegg <80585300+Flawlessegg@users.noreply.github.com>
2021-03-13 13:32:41 +0000

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,13 @@
+start microsoftedge https://www.youtube.com/watch?v=axUHSZRnmDY
+start microsoftedge https://www.youtube.com/watch?v=axUHSZRnmDY
+start microsoftedge https://www.youtube.com/watch?v=axUHSZRnmDY
+start microsoftedge https://www.youtube.com/watch?v=EBAY_BIBChM
+start microsoftedge https://www.youtube.com/watch?v=EBAY_BIBChM
+start microsoftedge https://www.youtube.com/watch?v=EBAY_BIBChM
+start microsoftedge http://papertoilet.com/
+start microsoftedge https://findtheinvisiblecow.com/
+start microsoftedge http://cat-bounce.com/
+X=msgbox("Ur being hacked lol",32+0,"Ha Ha")
+X=msgbox("Ur being hacked lol",32+0,"Ha Ha")
+X=msgbox("Ur being hacked lol",32+0,"Ha Ha")
+X=msgbox("Ur being hacked lol",32+0,"Ha Ha")
