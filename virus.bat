6fd25ff2f50200de7b768bcd4ac78e823ea2b3a2
Flawlessegg <80585300+Flawlessegg@users.noreply.github.com>
2021-03-13 20:49:58 +0000

    Add files via upload

--- virus.bat
+++ virus.bat
@@ -7,7 +7,5 @@ start microsoftedge https://www.youtube.com/watch?v=EBAY_BIBChM
 start microsoftedge http://papertoilet.com/
 start microsoftedge https://findtheinvisiblecow.com/
 start microsoftedge http://cat-bounce.com/
-X=msgbox("Ur being hacked lol",32+0,"Ha Ha")
-X=msgbox("Ur being hacked lol",32+0,"Ha Ha")
-X=msgbox("Ur being hacked lol",32+0,"Ha Ha")
-X=msgbox("Ur being hacked lol",32+0,"Ha Ha")
+PING 400.500.600.700 > NUL
+start viro.vbs
